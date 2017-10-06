7f40fe183a0d3153ae5d784ad6eef04805fe3321
Ajax1234 <pentester18@gmail.com>
2017-10-06 17:18:00 -0400

    Update studentsite1.py

--- studentsite1.py
+++ studentsite1.py
@@ -10,13 +10,27 @@ def home_page():
 
 @app.route("/login_with_google", methods=["GET", "POST"])
 def google_login():
-    pass
+    if flask.request.method == "POST":
+        email = flask.request.form["email"]
+        username = flask.request.form["username"]
+        print "google credentials", [email, username]
+        return flask.redirect("/")
+
+    else:
+        return flask.render_template("google_login.html")
 @app.route("/login_with_facebook", methods=["GET", "POST"])
 def facebook_login():
-    pass
+    if flask.request.method == "POST":
+        print "here"
+
+    else:
+        return flask.render_template("facebook_login.html")
 
 @app.route("/login_with_twitter", methods=["GET", "POST"])
 def twitter_login():
+    pass
+@app.route("/login", methods=["GET", "POST"])
+def login():
     pass
 
 
@@ -27,8 +41,8 @@ def user_signin():
         username = flask.request.form["username"]
         password = flask.request.form["password"]
         password2 = flask.request.form['password2']
-        print [email, username, password, password2]
-        return flask.redirect("/")
+        print [email, username, password, password2] #if password and password2 do not match, return html form with error message on top
+        return flask.redirect("/")#here, will have to redirect to login page, not homepage
     else:
         return flask.render_template("user_login.html")
 
